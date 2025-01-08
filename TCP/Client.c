586efc6738ca72faede6b0f46c16730b58051638
Arun <arunmundakkal003@gmail.com>
2025-01-08 22:43:57 +0530

    array

--- TCP/Client.c
+++ TCP/Client.c
@@ -1,39 +1,57 @@
-// Client
+
+// CLIENT
 
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <stdio.h>
 #include <arpa/inet.h>
+#include <unistd.h>
+#include <stdlib.h>
 
-
-int main(){
-
-    int socketResponse=socket(AF_INET,SOCK_STREAM,0);
+int main() {
+    int socketResponse = socket(AF_INET, SOCK_STREAM, 0);
 
     struct sockaddr_in client;
 
-    if(socketResponse == -1){
-
-        printf("error");
-    }else{
-
-        printf("\n socket: %d\n",socketResponse);
-
-        client.sin_family = AF_INET;
-        client.sin_addr.s_addr = inet_addr("127.0.0.1");
-        client.sin_port = htons(8888);
-
-        if(connect(socketResponse,(struct sockaddr *)&client, sizeof(client)) < 0){
-
-            printf("error connect\n");
-
-        }else{
-
-            printf("connect %d\n",client.sin_port);
-        }
-
-
+    if (socketResponse == -1) {
+        printf("Error creating socket\n");
+        return -1;
     }
 
+    printf("Socket created: %d\n", socketResponse);
 
+    client.sin_family = AF_INET;
+    client.sin_addr.s_addr = inet_addr("127.0.0.1");
+    client.sin_port = htons(8888);
+
+    if (connect(socketResponse, (struct sockaddr *)&client, sizeof(client)) < 0) {
+        printf("Error connecting\n");
+        return -1;
+    }
+
+    printf("Connected to server\n");
+
+    int arr[] = {5, 2, 9, 1, 6};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    // Send size of the array
+    write(socketResponse, &size, sizeof(size));
+
+    // Send array elements
+    write(socketResponse, arr, sizeof(int) * size);
+
+    printf("Array sent to server\n");
+
+    // Receive sorted array
+    read(socketResponse, arr, sizeof(int) * size);
+
+    printf("Sorted array received: ");
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    close(socketResponse);
+
+    return 0;
 }
--- TCP/Server.c
+++ TCP/Server.c
@@ -1,62 +1,90 @@
-
-//SERVER
+// SERVER
 
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <stdio.h>
 #include <arpa/inet.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main(){
-
-    int socketResponse=socket(AF_INET,SOCK_STREAM,0);
-
-    struct sockaddr_in saddr,caddr;
-
-
-    if(socketResponse == -1){
-
-        printf("error socket");
-
-    }else{
-        printf("\n socket: %d\n",socketResponse);
-
-        saddr.sin_family = AF_INET;
-        saddr.sin_addr.s_addr = inet_addr("127.0.0.1");
-        saddr.sin_port = htons(8888);
-        //bind
-        if(bind(socketResponse,(struct sockaddr*)&saddr,sizeof(saddr)) < 0){
-
-            printf("error in bind\n");
-
-        }else{
-            
-            printf("bind\n");
-
-            if(listen(socketResponse,5) < 0){
-
-                printf("error listen\n");
-
-            }else{
-
-                printf("listening \n");
-                //accept
-                int clen=sizeof(caddr);
-
-                int isock = accept(socketResponse,(struct sockaddr*) &caddr,&clen);
-
-                if(isock < 0){
-
-                    printf("error acceping\n");
-
-                }else{
-
-                    printf("accepted  \n port: %d\n",saddr.sin_port);
-
-                }
-
+void sortArray(int *arr, int size) {
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = 0; j < size - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
             }
         }
     }
+}
 
+int main() {
+    int socketResponse = socket(AF_INET, SOCK_STREAM, 0);
 
+    struct sockaddr_in saddr, caddr;
+
+    if (socketResponse == -1) {
+        printf("Error creating socket\n");
+        return -1;
+    }
+
+    printf("Socket created: %d\n", socketResponse);
+
+    saddr.sin_family = AF_INET;
+    saddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    saddr.sin_port = htons(8888);
+
+    // Bind
+    if (bind(socketResponse, (struct sockaddr *)&saddr, sizeof(saddr)) < 0) {
+        printf("Error binding\n");
+        return -1;
+    }
+
+    printf("Bind successful\n");
+
+    if (listen(socketResponse, 5) < 0) {
+        printf("Error listening\n");
+        return -1;
+    }
+
+    printf("Listening for connections\n");
+
+    int clen = sizeof(caddr);
+    int isock = accept(socketResponse, (struct sockaddr *)&caddr, &clen);
+
+    if (isock < 0) {
+        printf("Error accepting connection\n");
+        return -1;
+    }
+
+    printf("Connection accepted\n");
+
+    int arr[100], size;
+
+    // Receive size of the array
+    read(isock, &size, sizeof(size));
+
+    // Receive array elements
+    read(isock, arr, sizeof(int) * size);
+
+    printf("Received array: ");
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    // Sort the array
+    sortArray(arr, size);
+
+    // Send sorted array back
+    write(isock, arr, sizeof(int) * size);
+
+    printf("Sorted array sent back to client\n");
+
+    close(isock);
+    close(socketResponse);
+
+    return 0;
 }
